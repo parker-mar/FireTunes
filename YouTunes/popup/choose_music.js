13a41d17912d76db33442d7e647b48e0fe345712
Thomas <thomasleong3@gmail.com>
2016-03-15 01:13:59 -0500

    Added basic functions of YouTunes. v0.01

---
+++ YouTunes/popup/choose_music.js
@@ -0,0 +1,32 @@
+function musicNameToURL(beastName) {
+  switch (beastName) {
+    case "Youtube":
+      return chrome.extension.getURL("icons/youtube.jpg");
+    case "Pandora":
+      return chrome.extension.getURL("icons/youtube.jpg");
+    case "Spotify":
+      return chrome.extension.getURL("icons/youtube.jpg");
+  }
+}
+
+document.addEventListener("click", function(e) {
+  if (!e.target.classList.contains("music")) {
+    return;
+  }
+
+  var chosenMusic = e.target.textContent;
+  var chosenMusicURL;
+  switch (chosenMusic) {
+    case "Youtube":
+      chosenMusicURL = "http://www.youtube.com";
+      break;
+    case "Pandora":
+      chosenMusicURL = "http://www.pandora.com";
+      break;
+    case "Spotify":
+      chosenMusicURL = "http://www.spotify.com";
+      break;
+  }
+
+  window.location = chosenMusicURL;
+});
