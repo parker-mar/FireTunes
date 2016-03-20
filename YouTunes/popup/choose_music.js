1c4f182958e5d479442318cb2dba928c65b12c6d
wilsonsun <wilson.sun@hotmail.ca>
2016-03-20 17:53:26 -0400

    Update UI front end

--- YouTunes/popup/choose_music.js
+++ YouTunes/popup/choose_music.js
@@ -9,24 +9,44 @@ function musicNameToURL(beastName) {
   }
 }
 
-document.addEventListener("click", function(e) {
-  if (!e.target.classList.contains("music")) {
-    return;
-  }
+// document.addEventListener("click", function(e) {
+//   if (!e.target.classList.contains("music")) {
+//     return;
+//   }
 
-  var chosenMusic = e.target.textContent;
-  var chosenMusicURL;
-  switch (chosenMusic) {
+//   var chosenMusic = e.target.textContent;
+//   var chosenMusicURL;
+//   switch (chosenMusic) {
+//     case "Youtube":
+//       chosenMusicURL = "http://www.youtube.com";
+//       break;
+//     case "Pandora":
+//       chosenMusicURL = "http://www.pandora.com";
+//       break;
+//     case "Spotify":
+//       chosenMusicURL = "http://www.spotify.com";
+//       break;
+//   }
+  
+//   //window.location = chosenMusicURL;
+//    chrome.tabs.create({ url: chosenMusicURL });
+// });
+
+function musicButton(channel) {
+  switch (channel) {
     case "Youtube":
+      console.log("Youtube");
       chosenMusicURL = "http://www.youtube.com";
       break;
     case "Pandora":
+      console.log("Pandora");
       chosenMusicURL = "http://www.pandora.com";
       break;
     case "Spotify":
+      console.log("Spotify");
       chosenMusicURL = "http://www.spotify.com";
       break;
   }
-  
   window.location = chosenMusicURL;
-});
+
+}
